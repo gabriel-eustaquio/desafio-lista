d30fbacce33d41fae4e5c1b943afac9e7572529b
gabriel-eustaquio <gabriel.s.eustaquio@gmail.com>
2024-09-20 14:53:30 -0300

    :lipstick: feat: Correção

--- src/components/CardItem/cardItem.tsx
+++ src/components/CardItem/cardItem.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import styles from "@/components/CardItem/styles.module.scss";
 import { Task } from "../Card/card";
 import Image from "next/image";
@@ -16,69 +16,67 @@ type CardProps = {
 
 export default function CardItem({tasks, setTasks, tasksChecked, setTasksChecked, id, setId, setModalDelete, setTaskToDelete}: CardProps) {
 
-  function removeIdTasksChecked(e: React.MouseEvent<HTMLElement> | React.TouchEvent<HTMLElement>) {
-    setTasksChecked(tasksChecked.filter((task) => task.text != e.currentTarget.innerText));
+  function removeIdTasksChecked(taskText: string) {
+    setTasksChecked(tasksChecked.filter((task) => task.text !== taskText));
     setId(id + 1);
-    setTasks([...tasks, {text: e.currentTarget.innerText, id}]);
+    setTasks([...tasks, {text: taskText, id}]);
   }
 
-  function addToTasksChecked(e: React.MouseEvent<HTMLElement> | React.TouchEvent<HTMLElement>) {
+  function addToTasksChecked(taskText: string) {
     setId(id + 1);
-    setTasksChecked([...tasksChecked, {text: e.currentTarget.innerText.replace("Apagar", " ").trim(), id}])
-    setTasks(tasks.filter(task => task.text != e.currentTarget.innerText.replace("Apagar", " ").trim()))
+    setTasksChecked([...tasksChecked, {text: taskText, id}]);
+    setTasks(tasks.filter(task => task.text !== taskText));
   }
+
+  const handleDeleteClick = (taskText: string) => {
+    setTaskToDelete(taskText);
+    setModalDelete(true);
+  };
 
   return (
     <>
       <div>
         <h2>Suas tarefas de hoje</h2>
-        <ul className={`${styles.cardItem}`}>
+        <ul className={styles.cardItem}>
           {tasks.map((task) => {
             if (task.text) {
-              return <li key={`task${task.text + task.id}`} onClick={addToTasksChecked} onTouchStart={addToTasksChecked}>
-                <input type="checkbox" id={`${task.text + id}`} />
-                <label htmlFor={`${task.text + id}`}>{task.text}</label>
-                <div onClick={(e) => {
-                  setTaskToDelete(`${e.currentTarget.previousElementSibling?.innerHTML}`);
-                  setModalDelete(true);
-                  e.stopPropagation();
-                }} onTouchStart={(e) => {
-                  setTaskToDelete(`${e.currentTarget.previousElementSibling?.innerHTML}`);
-                  setModalDelete(true);
-                  e.stopPropagation();
-                }}> 
-                  <Image src="/imagens/binpoint.svg" width={24} height={24} alt="Logo" priority/>                  
-                </div>
-                
-              </li>
+              return (
+                <li key={`${task.text}${task.id}`}>
+                  <input type="checkbox" id={`${task.text}${id}`} />
+                  <label htmlFor={`${task.text}${id}`} onClick={() => addToTasksChecked(task.text)}>
+                    {task.text}
+                  </label>
+                  <div onClick={() => handleDeleteClick(task.text)} onTouchEnd={() => handleDeleteClick(task.text)}>
+                    <Image src="/imagens/binpoint.svg" width={24} height={24} alt="Logo" priority />
+                  </div>
+                </li>
+              );
             }
+            return null;
           })}
         </ul>
       </div>
       <div>
         <h2>Tarefas finalizadas</h2>
-        <ul className={`${styles.cardItem}`}>
-        {tasksChecked.map((task) => {
+        <ul className={styles.cardItem}>
+          {tasksChecked.map((task) => {
             if (task.text) {
-              return <li key={`task${task.text + task.id}`} onClick={removeIdTasksChecked}>
-                <input type="checkbox" id={`${task.text + id}`} defaultChecked/>
-                <label className={`${styles.tasksChecked}`} htmlFor={`${task.text + id}`}>{task.text}</label>
-                <div onClick={(e) => {
-                  setTaskToDelete(`${e.currentTarget.previousElementSibling?.innerHTML}`);
-                  setModalDelete(true);                  
-                  e.stopPropagation();
-                }} onTouchStart={(e) => {
-                  setTaskToDelete(`${e.currentTarget.previousElementSibling?.innerHTML}`);
-                  setModalDelete(true);                  
-                  e.stopPropagation();
-                }}>
-                  <Image src="/imagens/binpoint.svg" width={24} height={24} alt="Logo" priority/>                  
-                </div>
-              </li>
+              return (
+                <li key={`${task.text}${task.id}`}>
+                  <input type="checkbox" id={`${task.text}${id}`} defaultChecked />
+                  <label className={styles.tasksChecked} htmlFor={`${task.text}${id}`} onClick={() => removeIdTasksChecked(task.text)}>
+                    {task.text}
+                  </label>
+                  <div onClick={() => handleDeleteClick(task.text)} onTouchEnd={() => handleDeleteClick(task.text)}>
+                    <Image src="/imagens/binpoint.svg" width={24} height={24} alt="Logo" priority />
+                  </div>
+                </li>
+              );
             }
+            return null;
           })}
         </ul>
       </div>
     </>
-  )
+  );
 }
